2349b24d1b704056bed1e01189f39b87d5ddd89d
Melanie <melanie.ramnauth@gmail.com>
2025-04-13 23:02:20 -0400

    assignment completed, each new line of code has a description

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -11,6 +11,7 @@ set -x
 
 mkdir analysis output
 touch README.md
+echo "# Project Name: DSI Consulting Inc." > README.md
 touch analysis/main.py
 
 # download client data
@@ -21,21 +22,35 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
+mkdir data #creates directory named data
 
 # 2. Move the ./rawdata directory to ./data/raw
+mv ./rawdata ./data/raw #move rawdata directory to raw directory
 
 # 3. List the contents of the ./data/raw directory
+ls ./data/raw #list contents of subdirectory
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+cd data #move to data directory
+mkdir processed #creates processed subdirectory in data directory
+cd ./processed #move into processed directory
+mkdir server_logs user_logs event_logs #creates server_logs, user_logs, and event_logs directory
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
+cd .. #move back to data directory
+cp ./raw/*server*.log ./processed/server_logs/ #copies all files with "server" in the name, and carry a .log extension, to the .data/processed/server_logs destination folder
 
 # 6. Repeat the above step for user logs and event logs
+cp ./raw/*user*.log ./processed/user_logs/ 
+cp ./raw/*event*.log ./processed/event_logs/ #like line 44, except it copies files to event_logs
 
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+rm ./raw/*ipaddr*.log #removes log files with "ipaddr"
+rm ./raw/*ipaddr*.txt #removes txt files with "ipaddr"
+rm ./processed/user_logs/*ipaddr*.log #removes log files with "ipaddr"
+rm ./processed/user_logs/*ipaddr*.txt #removes txt files with "ipaddr"
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+ls processed/* > inventory.txt 
 
 ###########################################
 
